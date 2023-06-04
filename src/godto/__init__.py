8bcc17ce264afae4c859d8e1a0c489ba417a59db
Louis Maddox <louismmx@gmail.com>
2023-06-04 21:46:26 +0100

    Linting nit bugfix

--- src/godto/__init__.py
+++ src/godto/__init__.py
@@ -6,8 +6,7 @@ __all__ = []
 
 __author__ = "Louis Maddox"
 __license__ = "MIT"
-__description__ = "Data Transfer Object (DTO) codegen from JSON schemas following the
-OpenAPI spec"
+__description__ = "Data Transfer Object (DTO) codegen from JSON schemas following the OpenAPI spec"
 __url__ = "https://github.com/lmmx/godto"
 __uri__ = __url__
 __email__ = "louismmx@gmail.com"
